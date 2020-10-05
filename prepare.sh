101c32974fa2680af01e6aa79b4a923d2241e0fc
Mario Ceresa <mrceresa@gmail.com>
2020-10-05 12:59:38 +0200

    Fixed conda env activation

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,7 @@
 
+## Create iper environment
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
 conda create -n iper -y python=3.8 fiona geopandas matplotlib ipython numpy scipy pandas lxml cython coloredlogs seaborn
 conda activate iper
 
@@ -6,7 +9,10 @@ pip install contextily
 pip install git+https://github.com/projectmesa/mesa#egg=mesa
 pip install git+https://github.com/Corvince/mesa-geo.git#egg=mesa-geo
 
+## Install iper in devel mode
 pip install -e .
 
+## Install packages needed for examples:
+# SARS-COVID
 pip install covid19dh lmfit==1.0.0
 
