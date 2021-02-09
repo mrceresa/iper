c9c39d084c5e8a8460b67c447031d827dea3dab5
EudaldSabater <eudald.sabater@gmail.com>
2021-02-09 18:28:09 +0100

    Trajectories first implementation

--- examples/SAR-COV2/BCNCovid2020.py
+++ examples/SAR-COV2/BCNCovid2020.py
@@ -1,5 +1,6 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+from mesa.space import NetworkGrid
 
 from mesa_geo.geoagent import GeoAgent, AgentCreator
 
@@ -23,6 +24,7 @@ import json
 from iper import GeoSpacePandas
 
 import logging
+logging.basicConfig(level = logging.DEBUG)
 _log = logging.getLogger(__name__)
 
 class VirusInformation(object):
@@ -55,7 +57,7 @@ class Human(GeoAgent):
   def __repr__(self):
     return "Agent " + str(self.unique_id)
     
-    
+
 class BCNCovid2020(Model):
 
   def __init__(self, N, basemap):
@@ -64,13 +66,19 @@ class BCNCovid2020(Model):
     
     self._basemap = basemap
     self.grid = GeoSpacePandas()
+    self.transport_grid = NetworkGrid()
     self.schedule = RandomActivation(self)
     self.initial_outbreak_size = 100
     self.virus = VirusInformation()
 
     _log.info("Loading shapefiles")
-
     self.loadShapefiles()
+
+    _log.info("Loading Trajectories")
+    self.load_trajectories()
+    self.start_times = []
+    self.get_start_times_sorted()
+    self.traj_count = 0
     
     _log.info("Initalizing agents")
     self.createAgents(N)   
@@ -80,8 +88,7 @@ class BCNCovid2020(Model):
       self.grid.update_shape(agent, loc)
 
   def createAgents(self, N):
-      
-    base = self._xs["centroid"]
+    #base = self._xs["centroid"]
     AC = AgentCreator(Human, {"model": self})
     agents = []
     for i in range(N):
@@ -96,7 +103,42 @@ class BCNCovid2020(Model):
     self.grid.add_agents(agents)
     for agent in agents:
       self.schedule.add(agent)
-     
+
+  #Maybe will not be used....
+  def createTransportAgents(self, traj_list, transport_type): 
+    #traj_list: list of integers being the id of the trajectories in collection
+    AC = AgentCreator(transport_type , {"model", self})
+    agents = []
+    for i in traj_list:
+      _a = AC.create_agent( 
+        Point( 
+          collection.get_trajectory(i).get_start_location().x,
+          collection.get_trajectory(i).get_start_location().y
+        ), i)
+      agents.append(_a)
+
+    _log.info("Adding %d agent... of type ......."%len(agents))
+    self.transport_grid.add_agents(agents)
+    for agent in agents: 
+      self.schedule.add(agent)
+
+  def createTransportAgent(self, trajectory, traj_id, transport_type): 
+    AC = AgentCreator(transport_type , {"model", self})
+    _a = AC.create_agent( 
+        Point( 
+          trajectory.get_start_location().x,
+          trajectory.get_start_location().y
+        ), traj_id)
+
+    self.transport_grid.add_agents(_a)
+    self.schedule.add(_a)
+
+  def removeTransportAgents(self):
+    # Two options:
+    # 1: Create a get_end_times and perform like the creation of instances but to eraise them, 
+    # 2: Check when ever they arrived at its end location and eraise them then. 
+    pass
+
   def plotAll(self):
 
     fig = plt.figure(figsize=(15, 15))
@@ -144,14 +186,133 @@ class BCNCovid2020(Model):
     roads_2 = gpd.read_file(os.path.join(path, "shapefiles","2","roads-line.shp"))
     self._roads = [roads_1, roads_2]
 
+  def load_trajectories(self):
+    _log.info("Loading GTFS data")
+    trips = pd.read_csv(ROOT_DIR + '/GTFS/bus_metro/trips.txt', dtype = str).head(1)
+    routes = pd.read_csv(ROOT_DIR + '/GTFS/bus_metro/routes.txt', dtype = str)
+    st = pd.read_csv(ROOT_DIR + "/GTFS/bus_metro/stop_times.txt", dtype = str)
+    stops = pd.read_csv(ROOT_DIR + "/GTFS/bus_metro/stops.txt", dtype = str)
+    calendar = pd.read_csv(ROOT_DIR + "/GTFS/bus_metro/calendar_dates.txt", dtype = str)
+    _log.info("GTFS data Loaded")
+
+    # Add Geometry
+    stops = gpd.GeoDataFrame(stops, geometry=gpd.points_from_xy(stops.stop_lon, stops.stop_lat))
+    _log.info("Geometry added")
+
+    # Pass to datetime.date
+    t_dates = []
+    for item in calendar['date']:
+        date = datetime(year=int(item[0:4]), month=int(item[4:6]), day=int(item[6:8]))
+        t_dates.append(date)        
+
+    calendar['t_date'] = t_dates
+    _log.info("date format changed")
+
+    # Join together arrival and departure times
+    st.loc[st['trip_id'] == '1.1.2373316']
+    a = st[['trip_id','arrival_time', 'stop_id', 'stop_sequence']]
+    b = st[['trip_id','departure_time', 'stop_id', 'stop_sequence']]
+    a.loc[:,'time'] = a.loc[:,'arrival_time']
+    b.loc[:,'time'] = b.loc[:,'departure_time']
+    st = pd.concat([a,b])
+    st = st.drop(columns=['arrival_time', 'departure_time'])
+    st.sort_values(['trip_id', 'stop_id'])
+
+    # Pass to datetime.time
+    t = []
+    for each_time in st['time']:
+        try:
+            h, m, s = each_time.split(':')
+            each_t_time = timedelta(hours=int(h),minutes=int(m),seconds=int(s))
+            t.append(each_t_time)
+        except:
+            if each_time is np.nan: 
+                t.append(np.nan)
+            else:
+                print('error on a_time')
+                break
+
+    st['t_time'] = t
+    _log.info("time format changed")
+
+    # Merge the tables to get the desired attributes. 
+    stops_by_route = pd.merge(pd.merge(pd.merge(trips[['trip_id','route_id','service_id']],
+                                        st[['trip_id','stop_id','stop_sequence','t_time']]),
+                                        stops[['stop_id','geometry']]),
+                                        routes[['route_id', 'route_type']])
+    stops_by_route_date = pd.merge(stops_by_route,calendar[['service_id','t_date']])
+    _log.info("Tables merged")
+
+    # Group the trips by trip_id which are the trayectories on global dates. --- Expected lenght 47967 == # trips
+    grouped = stops_by_route_date.groupby(stops_by_route_date.trip_id)
+
+    # For each trip get all the days its running
+    traj_id = 0
+    trip_num = 0
+    count = 0
+    traj_list = []
+    for t_id in trips.trip_id:
+        trajectories_in_trip = grouped.get_group(t_id)
+        dates = trajectories_in_trip.loc[:,'t_date'].unique()
+        grouped_by_date = trajectories_in_trip.groupby(trajectories_in_trip.t_date) 
+
+        for day in dates:
+            trajectory = grouped_by_date.get_group(day)
+            trajectory.loc[:,'t'] = trajectory.loc[:,'t_time'] + trajectory.loc[:,'t_date']
+            trajectory.set_index('t', inplace=True)
+            gdf_trajectory = gpd.GeoDataFrame(trajectory, crs=CRS(31256))
+            traj = mpd.Trajectory(gdf_trajectory, traj_id)
+            traj_list.append(traj)
+            #traj.plot()
+        
+            traj_id += 1
+            if traj_id%100 == 0:
+                print(traj_id)
+                
+        trip_num += 1
+        if trip_num%100 == 0:
+            print('trip num is', trip_num)
+            
+        count += 1
+        if count == 5:
+            break
+
+    self.collection = mpd.TrajectoryCollection(traj_list)
+
+  def get_start_times_sorted(self):
+    for traj_id, trajectory in enumerate(self.collection):
+      self.start_times.append([trajectory.get_start_time(),traj_id])
+    sorted(self.start_times)
+    
+  def getStartingTrajectories(self, datetype):
+    while self.traj_count < len(self.start_times): 
+      if self.datetime == self.start_times[self.traj_count][0]:
+        trajectory = collection.get_trajectory(self.start_times[self.traj_count][1])
+        route_type = trajectory.df['route_type'].unique()
+        if route_type == 1:
+          self.createTransportAgent(trajectory, self.traj_count, Subway)
+        elif route_type == 3:
+          self.createTransportAgent(trajectory, self.traj_count, Bus)
+        elif route_type == 7:
+          self.createTransportAgent(trajectory, self.traj_count, Funicular)
+        else:
+          _log.warning("Transport type error, id: %d"%(self.traj_count))
+            
+        self.traj_count += 1
+      else: 
+        break
+
   def step(self):
+    #Somehow I need the time for the step. 
+    self.getStartingTrajectories() 
+    #self.createTransportAgents(traj_list = ,transport_type = ) Inside getStartingTrajectories
     self.schedule.step()
+    self.removeTransportAgents()
 
   def run_model(self, n):
     for i in range(n):
       _log.info("Step %d of %d"%(i, n))
       self.step()
 
-    
     
 
--- examples/SAR-COV2/Mobility.py
+++ examples/SAR-COV2/Mobility.py
@@ -1,8 +1,36 @@
-from mesa import Agent
 from mesa_geo.geoagent import GeoAgent
 import pygtfs
+import os
 
-class Agent(GeoAgent):
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+import movingpandas as mpd
+from datetime import datetime, timedelta
+import numpy as np
+from pyproj import CRS
+
+import logging
+_log = logging.getLogger(__name__)
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+
+# Functions that may be needed
+def load_merged_GTFS(self):
+    self.merged_GTFS = pd.read_csv((ROOT_DIR + '/GTFS/merged_GTFS.csv'), dtype = str)
+def save_merged_GTFS(self):
+    self.merged_GTFS.to_csv(ROOT_DIR + '/GTFS/merged_GTFS.csv')
+class pygtfs_Schedule():
+    def __init__(self):
+        sched = pygtfs.Schedule(":memory:")
+        pygtfs.append_feed(sched, "GTFS/bus_metro")
+        print(sched.agencies)
+
+    def now(date, time):
+        self.date = date
+        self.time = time
+# -----------------------------------------   
+class TransportAgent(GeoAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)
 
@@ -12,7 +40,10 @@ class Agent(GeoAgent):
     def get_pos(self):
         return (self.shape.x, self.shape.y)
 
-class Human(Agent):
+    def get_stops(self, trajectory):
+        return trajectory.df.geometry.unique()
+
+class Human(GeoAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)
         # Markov transition matrix
@@ -32,61 +63,92 @@ class Human(Agent):
         self.place_at(newPosNode)
         #neighbors = self.model.grid.get_neighbors(self)
 
-class Tram(Agent):
+class Tram(TransportAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)
+        max_capacity = 100
 
     def step(self):
         pass
 
-class Subway(Agent):
+class Subway(TransportAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)
-    def step(self):
-        pass
+        self.max_capacity = 500
+        self.passangers = []
+        self.trajectory = self.collection.get_trajectory(unique_id)
+        self.stops = self.get_stops(self.trajectory)
+        self.currentStation = 0
 
-class Bus(Agent):
+    def step(self):
+        currentPos = self.get_pos()
+        newPos = self.trajectory.get_pos(model.time)  #time from the main
+        self.place_at(newPos)
+
+        if self.stops[currentStation].x == newPos.x and self.stops[currentStation].y == newPos.y:
+            #People can enter
+
+        #Check whether the station has changed
+        if self.stops[currentStation].x == currentPos.x and self.stops[currentStation].y == currentPos.y and 
+        self.stops[currentStation].x != newPos.x and self.stops[currentStation].y != newPos.y:
+            self.currentStation += 1
+        else:
+            # We are still in the station
+
+
+
+class Bus(TransportAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)   
     def step(self):
         pass
 
-class Car(Agent):
+class Car(TransportAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)
 
     def step(self):
         pass
 
-class Bike(Agent):
+class Bike(TransportAgent):
     def __init__(self, unique_id, model, shape):
         super().__init__(unique_id, model, shape)
     def step(self):
         pass
-    
-class Stops():
-    pass
-
-class Schedule():
-    def __init__(self, date, time):
-        self.date = date
-        self.time = time
-
-    def now(date, time):ç
-        pass
-    
-
-if __name__ == "__main__":
-    sched = pygtfs.Schedule(":memory:")
-    pygtfs.append_feed(sched, "GTFS/bus_metro")
-    print(sched.agencies)
 
 
-    
 # DISCUSS
-# WORK AS TRAJECTORIES PRELOADED OR CHECK TIME AND READ FEED 
+# Work done: 
+    # Download GTFS
+    # Transform GTFS to Trajectories.
+    # Collect them in a collection
+    # Get the starting and ending times of each trajectory
+    # If the starting time of a trajectory coincides with the current time, create a new
+    # instance of the correpsonding transport type.
+    # Made the transport subway class 
+    # Try to add humans to the transport.
+
+
+# doubts: 
+# Need to deal with more than 86.000.000 of trajectories
+# Now working with steps, i need time to make trjectories work. 
+# I init each trajectory inside model.step. Is it okay there? 
+# Remove still think how to do it explenation in the function
+# schedule random activation? First transports then humans. 
+    # Maybe if we take into account seconds as time unit does not matter because we are in one stop for more 
+    # than one step. 
+
+
+# Is the structure good? 
+# Add persons to transport_grid and therefore transport?
+    # Get from the human_grid, the humans that are neigbours to the station location and then 
+    # if they will, move them to the passangers list. How to add them into the tramsport grid?
+
+
+
+# Check network grid .... tests!!!!
+
+
 # HOW TIME WORKS
 # TRAM FEED IS NOT VALID. SUGESTION: WORK FIRST WITH SUBWAY AND BUS AND THEN FIGURE OUT HOW TO WORK WITH TRAM
-# WORKPLAN, NECESSARY TO BE IN THE PROJECT REPORT? 
-
-    
+# WORKPLAN, NECESSARY TO BE IN THE PROJECT REPORT?    
