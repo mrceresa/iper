7b8069e327bdd4b3efa70eb5cfa4e6ad5a558646
AranColl <aran.coll01@estudiant.upf.edu>
2021-03-17 16:10:32 +0100

    Differentiation between symptomatic and asymtpomatic infected people. PCR testing and contact tracing, and self quarantine of agents. It does work but some more details should be added.

--- examples/Aran Covid/Covid_class.py
+++ examples/Aran Covid/Covid_class.py
@@ -1,35 +1,57 @@
-from enum import IntEnum
+from enum import Enum
 
 
 class VirusCovid(object):
-    def __init__(self, len_timetable):
+    def __init__(self):
         self.r0 = 2.5
 
         # from EXP to INF
-        self.incubation_days = 2 * len_timetable  # by multiplying the length of the timetable, we get the total number of days in such state
-        self.incubation_days_sd = 1 * len_timetable
+        self.incubation_days = 2
+        self.incubation_days_sd = 1
 
         # from INF to REC - HOSP
-        self.infection_days = 21 * len_timetable
-        self.infection_days_sd = 7 * len_timetable
+        self.infection_days = 5#1
+        self.infection_days_sd = 1#7
 
         # from REC to SUSC
-        self.immune_days = 30 * len_timetable
-        self.immune_days_sd = 10 * len_timetable
+        self.immune_days = 3#0
+        self.immune_days_sd = 1#0
 
         # from HOSP to REC - DEATH
-        self.severe_days = 20 * len_timetable
-        self.severe_days_sd = 10 * len_timetable
+        self.severe_days = 2#0
+        self.severe_days_sd = 1#0
 
-        self.ptrans = 0.5
-        self.death_rate = 0.02
-        self.severe_rate = 0.05
+        self.ptrans = 0.7
+        self.pSympt = 0.4  # probability to present the symptoms
+        self.pTest = 0.8  # probability of test of true positive
+        self.death_rate = 0.002 / (24 * 4)
+        self.severe_rate = 0.005 / (24 * 4)
 
 
-class State(IntEnum):
+class State(Enum):
     SUSC = 0
     EXP = 1
     INF = 2
     REC = 3
     HOSP = 4
     DEAD = 5
+
+    def __str__(self):
+        return self.name
+
+
+class Mask(Enum):
+    NONE = 0
+    HYGIENIC = 1
+    FFP2 = 2
+
+    def __str__(self):
+        return self.name
+
+    def maskPtrans(self):
+        if Mask.NONE:return 1
+        elif Mask.HYGIENIC: return 0.90
+        elif Mask.FFP2: return 0.80
+
+    def RandomMask(self):
+        return Mask.FFP2
--- examples/Aran Covid/Hospital_class.py
+++ examples/Aran Covid/Hospital_class.py
@@ -1,5 +1,7 @@
 from mesa import Agent
+from Covid_class import State, Mask
 import numpy as np
+from datetime import datetime, timedelta
 
 
 class Hospital(Agent):
@@ -7,8 +9,14 @@ class Hospital(Agent):
         super().__init__(unique_id, model)
         self.place = (self.random.randrange(self.model.grid.width), self.random.randrange(self.model.grid.height))
         self.total_capacity = int(len(self.model.schedule.agents) / 10)  # 10% of population
-        self.list_pacients = set()
+        self.list_pacients = set()  # patients in the hospital
+        self.PCR_availables = self.random.randrange(3, 5)
+        self.PCR_testing = {}  # patients waiting for the testing
+        self.PCR_results = {}  # patients tested
+        self.mask = Mask.FFP2
 
+    def __str__(self):
+        return "Hospital at " + str(self.place) + "with " + str(self.PCR_availables) + " available tests"
 
     def get_pacients(self):
         """ Returns hospital pacients """
@@ -26,6 +34,69 @@ class Hospital(Agent):
         """ Discharges a pacient """
         self.list_pacients.remove(agent)
 
+    def doTest(self, agent):
+        """ Tests an agent.
+        If result is positive agent contacts will be added to hospital PCR testing for future tests. """
+        print(f"Agente {agent.unique_id} testeandose en hospital {self.unique_id}")
+
+        today = self.model.DateTime.strftime('%Y-%m-%d')
+        if not today in self.model.peopleTested: self.model.peopleTested[today] = {agent}
+        else: self.model.peopleTested[today].add(agent)
+
+
+        agentStatus = agent.state
+        pTest = self.model.virus.pTest
+        true_pos = np.random.choice([True, False], p=[pTest, 1 - pTest])
+        if true_pos:  # test knows true state
+            if agentStatus == State.EXP or agentStatus == State.INF:
+                agentcontacts = agent.contacts
+                print(f"Resulta que es positivo, da sus contactos {agentcontacts}")
+                # save agent contacts for future tests
+                for key in agentcontacts:
+                    for elem in agentcontacts[key]:
+                        #if not today in self.PCR_testing:
+                        if not today in self.model.peopleToTest:
+                            self.model.peopleToTest[today] = {elem}
+                        else:
+                            self.model.peopleToTest[today].add(elem)
+
+        """if not today in self.PCR_results:
+            self.PCR_results[today] = {agent}
+        else:
+            self.PCR_results[today].add(agent)"""
+            # adds agents to list of people tested in model
+
+    def decideTesting(self, patientsTested):
+        PCRs = self.PCR_availables
+        HospToTest = set()
+
+        ThreeD_ago = (self.model.DateTime - timedelta(days=3)).strftime('%Y-%m-%d')
+
+        if ThreeD_ago in self.model.peopleToTest and PCRs:
+            for elem in self.model.peopleToTest[ThreeD_ago]:
+                if elem not in patientsTested and PCRs:
+                    PCRs -= 1
+                    HospToTest.add(elem)
+                    elem.quarantine = self.model.DateTime
+                    elem.obj_place = self.place
+
+            self.model.peopleToTest[ThreeD_ago] -= HospToTest
+
+
+        TwoD_ago = (self.model.DateTime - timedelta(days=2)).strftime('%Y-%m-%d')
+        if PCRs and TwoD_ago in self.model.peopleToTest:
+            for elem in self.model.peopleToTest[TwoD_ago]:
+                if elem not in (patientsTested | HospToTest) and PCRs:
+                    PCRs -= 1
+                    HospToTest.add(elem)
+                    self.quarantine = self.model.DateTime
+                    elem.obj_place = self.place
+
+            self.model.peopleToTest[TwoD_ago] -= HospToTest
+
+        print(f"Hoy el hospital {self.unique_id} testeara a {HospToTest}")
+        return HospToTest
+
 
 class Workplace(Agent):
     def __init__(self, unique_id, model):
@@ -33,11 +104,16 @@ class Workplace(Agent):
         self.place = (self.random.randrange(self.model.grid.width), self.random.randrange(self.model.grid.height))
         self.total_capacity = 0
         self.workers = set()
+        self.mask = Mask.NONE
+        self.mask = self.mask.RandomMask()
+
+    def __str__(self):
+        return "Workplace at " + str(self.place)
 
     def set_capacity(self, mean, sd):
         """ Sets capacity of the workplace. """
         self.total_capacity = int(np.ceil(self.random.normalvariate(mean, sd)))
-        #print("Capacity of workplace: " + str(self.unique_id) + " has a capacity of " + str(self.total_capacity))
+        # print("Capacity of workplace: " + str(self.unique_id) + " has a capacity of " + str(self.total_capacity))
 
     def add_worker(self, agent):
         """ Adds a worker to the workplace hospital pacients """
@@ -46,4 +122,3 @@ class Workplace(Agent):
     def get_workers(self):
         """ Returns workplace workers.  """
         return self.workers
-
