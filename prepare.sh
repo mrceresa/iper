353b915563f844a3057c1c3d348939deaa390eb6
Mario Ceresa <mrceresa@gmail.com>
2023-08-17 18:47:27 +0200

    Improved installation

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,20 @@
-
+#!\bin\bash
+set -xe
 ## Create iper environment
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
-conda create -c conda-forge -n iper -y python=3.8 fiona geopandas geoplot \
-	matplotlib ipython numpy scipy pandas lxml cython coloredlogs seaborn
+conda create -c conda-forge -n iper -y python=3.10 mamba
+
 conda activate iper
 
-pip install contextily trimesh meshio transitions
-pip install git+https://github.com/projectmesa/mesa#egg=mesa
-pip install git+https://github.com/Corvince/mesa-geo.git#egg=mesa-geo
+mamba install -c conda-forge -y matplotlib numpy scipy pandas seaborn coloredlogs lxml cython   
+mamba install -c conda-forge -y fiona geopandas geoplot pyproj rtree shapely descartes
+mamba install -c conda-forge -y contextily trimesh meshio transitions 
+
+#pip install contextily trimesh meshio transitions
+
+pip install -U mesa
+pip install -U mesa-geo
 
 ## Install iper in devel mode
 pip install -e .
